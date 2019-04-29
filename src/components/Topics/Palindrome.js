4099d8fe7287291c89e63c2274aece5b700c4328
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-29 16:01:01 -0600

    i hate palindrome

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react'
 
 class Palindrome extends Component {
+    constructor() {
+        super()
+        this.state ={
+            userInput: '',
+            palindrome: ''
+        }
+    }
+
+    handleUpdateUserInput(value) {
+        this.setState({userInput: value})
+    }
+
+    handleSolvePalindrome() {
+        let reversedString = this.state.userInput.split('').reverse().join('')
+        if(this.state.userInput === reversedString) {
+            this.setState({palindrome: `${this.state.userInput} is a palindrome`})
+        } else {
+            this.setState({palindrome: `${this.state.userInput} is not a palindrome`})
+        }
+    }
+
+
     render() {
         return(
-            <div>
-                <p>Palindrome Component</p>
+            <div className="puzzleBox filterStringPB">
+                <h4>Palindrome</h4>
+                <input className="inputLine" onChange={(e) => {this.handleUpdateUserInput(e.target.value)}}/>
+                <button className="confirmationButton" onClick={() => {this.handleSolvePalindrome()}}>Solve</button>
+                <span className="resultsBox">{this.state.palindrome}</span>
             </div>
         )
     }
